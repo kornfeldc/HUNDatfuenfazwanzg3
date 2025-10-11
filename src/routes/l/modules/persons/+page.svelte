f603d216956b8710d291d2656309966d6edcb5ee
kornfeldc <kornfeld.c@gmail.com>
2025-10-11 11:12:52 +0200

    Add async loading for persons module with a loading spinner and grid display

--- src/routes/l/modules/persons/+page.svelte
+++ src/routes/l/modules/persons/+page.svelte
@@ -1,3 +1,11 @@
 <script lang="ts">
+    import Loading from "$lib/components/global/Loading.svelte";
+    import PersonsGrid from "$lib/components/persons/PersonsGrid.svelte";
+
+    let {data}: { data: any } = $props();
 </script>
-person module
+{#await data.persons}
+    <Loading></Loading>
+{:then persons}
+    <PersonsGrid {persons} />
+{/await}
