0d5d91b57578191ab622ec8f6fb5ba1fea316a3c
kornfeldc <kornfeld.c@gmail.com>
2025-10-16 18:35:31 +0200

    Update Grid component to adjust column responsiveness and gaps for improved layout readability.

--- src/lib/components/global/Grid.svelte
+++ src/lib/components/global/Grid.svelte
@@ -1,6 +1,8 @@
 <script lang="ts">
     let { children, minColumnWidth = "16em",  className = "" } = $props();
 </script>
-<div class={"grid grid-cols-[repeat(auto-fit,minmax("+minColumnWidth+",1fr))] gap-2 sm:gap-4 mx-4 ${className}"}>
+
+<!--<div class={"grid grid-cols-[repeat(auto-fit,minmax("+minColumnWidth+",1fr))] gap-2 sm:gap-4 mx-4 ${className}"}>-->
+<div class={"grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-2 sm:gap-4 mx-2 sm:mx-4 ${className}"}>
     {@render children?.()}
 </div>
