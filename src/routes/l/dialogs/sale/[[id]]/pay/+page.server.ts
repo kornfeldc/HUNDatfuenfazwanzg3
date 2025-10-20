b1c1e491ee5962767b43ca90c92214b185578d6b
kornfeldc <kornfeld.c@gmail.com>
2025-10-20 18:01:23 +0200

    Introduce payment page for sale dialogs; add asynchronous data loading, form submission handling, and improved modular code structure with new Svelte components and server functions.

---
+++ src/routes/l/dialogs/sale/[[id]]/pay/+page.server.ts
@@ -0,0 +1,38 @@
+import {HfzApi} from "$lib/data/hfzApi";
+import {fail, redirect} from "@sveltejs/kit";
+import {Util} from "$lib/util";
+
+export async function load({cookies, params, url}) {
+    const {id} = params;
+    if (!id) return {title: 'Neuer Artikel'};
+    const api = HfzApi.create();
+    return {
+        sale: api.getSale({id: parseInt(id)}),
+    };
+}
+
+export const actions = {
+    default: async ({cookies, request, params}) => {
+        const {id} = params; // Extract the `id` parameter from the `params` object
+        const formData = await request.formData();
+
+        const redirectTo = formData.get('redirectTo')?.toString() ?? "/l/modules/articles";
+        formData.delete('redirectTo');
+
+        let data = Util.parseFormData(formData, [
+            ]);
+        if(id)
+            data.id = parseInt(id);
+        console.log("parsed data", data);
+
+        try {
+            // todo - save data via api
+        } catch (e: any) {
+            return fail(422, {
+                error: e.message
+            });
+        }
+
+        throw redirect(303, redirectTo);
+    }
+};
---
+++ src/routes/l/dialogs/sale/[[id]]/pay/+page.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+    import {page} from '$app/stores';
+    import type {ISale} from "$lib/data/hfzApi";
+    import Loading from "$lib/components/global/Loading.svelte";
+
+    let id = $page.params.id;
+    let {data}: { data: any; } = $props();
+
+    let sale = $state({} as ISale);
+
+    const loadData = async () => {
+        sale = await data.sale;
+    }
+</script>
+{#await loadData()}
+    <Loading></Loading>
+{:then _}
+    <form method="post" action="/l/dialogs/sale/{id}">
+        test {sale.personName}
+    </form>
+{/await}
