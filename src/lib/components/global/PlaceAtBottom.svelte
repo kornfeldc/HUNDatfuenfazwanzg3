ce26a034af423b252b537ba3ee7246919149eb85
kornfeldc <kornfeld.c@gmail.com>
2026-01-27 20:51:20 +0100

    Refine `PlaceAtBottom` styling: adjust conditional background opacity, simplify `bottomClass` logic, and clean up redundant condition handling.

--- src/lib/components/global/PlaceAtBottom.svelte
+++ src/lib/components/global/PlaceAtBottom.svelte
@@ -1,15 +1,17 @@
 <script lang="ts">
     import {Util} from "$lib/util";
     import {uiState} from "$lib/stores/uiState.svelte";
+
     let {children, at = 'left', top = false} = $props();
-    
-    let bottomClass = $derived.by(()=> {
-        return uiState.showPlaceAtBottomOnTop() || top ? "fixed top-0" : "fixed bottom-0";
-    } );
+
+    let bottomClass = $derived.by(() => {
+        return (uiState.showPlaceAtBottomOnTop() || top ? "fixed top-0" : "fixed bottom-0") + " "+
+            (top ? "bg-background/80" : "");
+    });
 </script>
-{#if uiState.showPlaceAtBottomOnTop() || top}
-    <div class="fixed top-0 w-full bg-background/90 shadow-background/80 shadow-lg blur-sm h-18"></div>
+{#if uiState.showPlaceAtBottomOnTop()}
+    <div class="fixed top-0 w-full bg-background/80 shadow-background/80 shadow-lg blur-sm h-18"></div>
 {/if}
-<div class={Util.mapClass(`${bottomClass} p-3 flex gap-2`, at === 'left', "left-0", "right-0")}>
+<div class={Util.mapClass(`${bottomClass} p-3 flex gap-2 `, at === 'left', "left-0", "right-0")}>
     {@render children?.()}
 </div>
